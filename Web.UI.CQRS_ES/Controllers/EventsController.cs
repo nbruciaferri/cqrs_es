077e83494ffca8770cf6ff46be64c5b030646e9d
niccolo <Niccolo.Bruciaferri@diatechpharmacogenetics.com>
2021-10-04 10:20:45 +0200

    Removed Web project

---
+++ Web.UI.CQRS_ES/Controllers/EventsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS_ES.ES;
+
+namespace Web.UI.CQRS_ES.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly EventsStore _eventStore;
+
+        public EventsController(EventsStore eventsStore)
+        {
+            _eventStore = eventsStore;
+        }
+
+        // get: client
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowProductsByAggregateId(Guid aggregateId)
+        {
+            var eventsByAggregateId = _eventStore.GetEventsByAggregate(aggregateId);
+            ViewData["NumEvents"] = eventsByAggregateId.Count;
+
+            if (eventsByAggregateId.Count > 0)
+            {
+                foreach (var @event in eventsByAggregateId)
+                {
+                    ViewData["AggregateId"] = @event.ToString();
+                }
+            }
+
+            return View();
+        }
+    }
+}
